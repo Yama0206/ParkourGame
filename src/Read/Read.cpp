07d3f1a10aac06b8ab26e23342ead245e5910fae
Yama0206 <hiyamakouta2@gmail.com>
2024-09-03 17:19:04 +0900

    csv up

---
+++ src/Read/Read.cpp
@@ -0,0 +1,110 @@
+#include "Read.h"
+
+//初期化
+void CRead::Init()
+{
+	memset(&m_Enemy, 0.0, sizeof(m_Enemy));
+
+
+	//X
+	for (int X = 0; X < MAP_CHIP_X_NUM; X++)
+	{
+		//Y
+		for (int Y = 0; Y < MAP_CHIP_Y_NUM; Y++)
+		{
+			m_FileReadMapData[Y][X] = 0;
+		}
+	}
+}
+
+
+//読み込み
+void CRead::ReadFile()
+{
+	FILE* fp;
+	fopen_s(&fp, "Data/Map/マップチップ.txt", "r");
+
+	int mapIndexX = 0;
+	int mapIndexY = 0;
+
+	int resurt = 0;
+
+	while (true) {
+		//1行目は使わないので先に読み込みだけ済ませる
+		/*fgets();*/
+
+		//数値の読み込み
+		fscanf_s(fp, "%d, %f, %f, %f, %f, %f, %f, %d", &eType, &m_Enemy.m_vPos.x, &m_Enemy.m_vPos.y, &m_Enemy.m_vPos.z, &m_Enemy.m_vSize.x, &m_Enemy.m_vSize.y, &m_Enemy.m_vSize.z, &eType); 
+		
+		//「,」を飛ばすために読み込み実行
+		char c = fgetc(fp);
+
+		//EOFの場合は読み込み終了
+		if (c == EOF) {
+			break;
+		}
+
+		//改行コードの場合は保存先を変更する
+		if (c == '\n') {
+			mapIndexY++;
+			mapIndexX = 0;
+		}
+		//改行でも読み込み終了でもなかったらプラス
+		else
+		{
+			mapIndexX++;
+		}
+
+	}
+
+	fclose(fp);
+}
+
+//ファイルの書き込み
+void CRead::SaveFile()
+{
+	//ファイル構造体宣言
+	FILE* fp;
+	//ファイルを開く
+	fopen_s(&fp, "Data/Map/マップチップ.csv", "a");
+
+	if (fp != NULL) {
+		int mapIndexX = 0;
+		int mapIndexY = 0;
+
+		while (true) {
+			//数値の読み込み
+			fprintf_s(fp, "%d", &m_FileReadMapData[mapIndexY][mapIndexX]);
+
+			//「,」を飛ばすために読み込み実行
+			char c = fgetc(fp);
+
+			//EOFの場合は読み込み終了
+			if (c == EOF) {
+				break;
+			}
+
+			//改行コードの場合は保存先を変更する
+			if (c == '\n') {
+				mapIndexY++;
+				mapIndexX = 0;
+				fprintf_s(fp, "\n");
+			}
+			//改行でも読み込み終了でもなかったらプラス
+			else
+			{
+				mapIndexX++;
+			}
+
+		}
+	}
+
+}
+
+//描画
+void CRead::Draw()
+{
+	int t = eType;
+
+	DrawFormatString(32, 32,GetColor(255, 0, 0), "%d, %f, %f, %f, %f, %f, %f, %d", &t, m_Enemy.m_vPos.x, m_Enemy.m_vPos.y, m_Enemy.m_vPos.z, m_Enemy.m_vSize.x, m_Enemy.m_vSize.y, m_Enemy.m_vSize.z, &t, true);
+}
---
+++ src/Read/Read.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "DxLib.h"
+#include <iostream>
+#include <vector>
+
+const int  MAP_CHIP_X_NUM = 10;		//マップチップ横の数
+const int  MAP_CHIP_Y_NUM = 1;		//マップチップ縦の数
+
+const int TYPE_NUM = 256;					//敵タイプの配列の数
+const int PATH_NUM = 256;					//敵パスの配列の数
+
+
+enum Type
+{
+	a = 1,
+	b, 
+	c,
+	d,
+};
+
+struct s_Enemy
+{
+	VECTOR m_vPos;
+	VECTOR m_vSize;
+
+	char m_cType[TYPE_NUM];
+	char m_cPath[PATH_NUM];
+};
+
+
+
+class CRead
+{
+private:
+	s_Enemy m_Enemy;
+	Type eType;
+
+	//マップチップが格納された変数
+	int m_FileReadMapData[MAP_CHIP_Y_NUM][MAP_CHIP_X_NUM];
+
+public:
+	//初期化
+	void Init();
+
+	//ファイル読み込み
+	void ReadFile();
+	//ファイルの書きこみ
+	void SaveFile();
+
+	//デバッグ用描画
+	void Draw();
+
+	//csvの値を取得
+	int GetFileReadMapData(int x_index, int y_index) { return m_FileReadMapData[y_index][x_index]; }
+
+
+};
